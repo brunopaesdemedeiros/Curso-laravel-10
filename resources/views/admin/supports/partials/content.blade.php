3d7ab58c0ac5f509d3c3e6108dfd88802975a013
Bruno Paes de Medeiros <brunopaesdemedeiros@gmail.com>
2024-08-15 22:50:55 -0300

    Ajuste layout supports.

--- resources/views/admin/supports/partials/content.blade.php
+++ resources/views/admin/supports/partials/content.blade.php
@@ -5,26 +5,26 @@
                 <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
                     <thead class="bg-gray-50 dark:bg-gray-800">
                         <tr>
-                            <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <th scope="col" class="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500 bg-gray-800 dark:text-gray-400">
                                 Assunto
                             </th>
 
-                            <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <th scope="col" class="px-12 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 bg-gray-800 dark:text-gray-400">
                                 Status
                             </th>
 
-                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 bg-gray-800 dark:text-gray-400">
                                 Comentário
                             </th>
 
-                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400">Interações</th>
+                            <th scope="col" class="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 bg-gray-800 dark:text-gray-400">Interações</th>
 
-                            <th scope="col" class="relative py-3.5 px-4">
+                            <th scope="col" class="relative py-3.5 px-4 bg-gray-800">
                                 <span class="sr-only">Ver</span>
                             </th>
                         </tr>
                     </thead>
-                    <tbody class="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                    <tbody class="bg-black text-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
                         @foreach ($supports->items() as $support)
                         <tr>
                             <td class="px-4 py-2 text-sm font-medium whitespace-nowrap dark:text-white">
